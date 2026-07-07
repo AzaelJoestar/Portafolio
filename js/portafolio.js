71622588dea737435194ba5d80584b28b1dbe140
AzaelJoestar <azael.h.uvera@gmail.com>
2026-07-06 22:15:55 -0600

    Add files via upload

---
+++ js/portafolio.js
@@ -0,0 +1,11 @@
+function irAProyectos() {
+    document.getElementById("proyectos").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function irAContacto() {
+    document.getElementById("contacto").scrollIntoView({
+        behavior: "smooth"
+    });
+}
